5aa49ca849512fd7aba9239c15e15c21f08643ee
Dario Ramos <dario.fenix@gmail.com>
2019-03-18 17:14:47 -0300

    [Warmup][03] Jumping on Clouds

---
+++ Warmup/03_JumpingOnClouds.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the jumpingOnClouds function below.
+func jumpingOnClouds(c []int32) int32 {
+	var jumpCount int32 = 0
+	var currentPos int32
+	var maxPos = int32(len(c)) - 1
+	for currentPos = 0; currentPos < maxPos; {
+		// Try to jump 2 spaces; else, jump 1, always checking not to go out of range
+		if currentPos+2 <= maxPos && c[currentPos+2] != 1 {
+			currentPos += 2
+			jumpCount++
+		} else {
+			currentPos++
+			jumpCount++
+		}
+	}
+	return jumpCount
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	cTemp := strings.Split(readLine(reader), " ")
+
+	var c []int32
+
+	for i := 0; i < int(n); i++ {
+		cItemTemp, err := strconv.ParseInt(cTemp[i], 10, 64)
+		checkError(err)
+		cItem := int32(cItemTemp)
+		c = append(c, cItem)
+	}
+
+	result := jumpingOnClouds(c)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
