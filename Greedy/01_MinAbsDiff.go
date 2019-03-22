cf12b55b1bcc8fb4319c425333310ee000a7caa5
Dario Ramos <dario.fenix@gmail.com>
2019-03-22 18:54:20 -0300

    [Greedy][01] Minimum absolute difference

---
+++ Greedy/01_MinAbsDiff.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+// Complete the minimumAbsoluteDifference function below.
+func minimumAbsoluteDifference(arr []int32) int32 {
+	sort.Slice(arr, func(i, j int) bool { return arr[i] < arr[j] })
+	var minAbsDiff int32 = math.MaxInt32
+	for i := 0; i < len(arr)-1; i++ {
+		absDiff := int32(math.Abs(float64(arr[i] - arr[i+1])))
+		if absDiff < minAbsDiff {
+			minAbsDiff = absDiff
+		}
+	}
+	return minAbsDiff
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	arrTemp := strings.Split(readLine(reader), " ")
+
+	var arr []int32
+
+	for i := 0; i < int(n); i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	result := minimumAbsoluteDifference(arr)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
